b1af3047e26614eae5bf88afc658fe6d47a41734
Shishir Dey <shishirdey241097@gmail.com>
2024-02-21 22:18:39 +0530

    Implement ModbusFrame* subclass with serialization and deserialization methods

---
+++ src/ModbusFrame.cpp
@@ -0,0 +1,71 @@
+#include "ModbusFrame.hpp"
+#include "Checksum.hpp"
+
+std::array<uint8_t, 256> ModbusFrame::serialize()
+{
+}
+
+void ModbusFrame::deserialize(const std::array<uint8_t, 256>& data)
+{
+}
+
+std::array<uint8_t, 256> ModbusRTUFrame::serialize()
+{
+    std::array<uint8_t, 256> data {};
+    size_t dataIndex = 0;
+
+    data[dataIndex++] = slaveaddr;
+    data[dataIndex++] = static_cast<uint8_t>(functionCode);
+
+    std::copy(frameData.begin(), frameData.end(), data.begin() + dataIndex);
+    dataIndex += frameData.size();
+
+    checksum = Checksum::calculateCRC16(data.data(), dataIndex);
+
+    data[dataIndex++] = checksum & 0xFF;
+    data[dataIndex++] = (checksum >> 8) & 0xFF;
+
+    return data;
+}
+void ModbusRTUFrame::deserialize(const std::array<uint8_t, 256>& data)
+{
+    size_t dataIndex = 0;
+
+    slaveaddr = data[dataIndex++];
+    functionCode = static_cast<ModbusFunctionCode>(data[dataIndex++]);
+
+    size_t frameDataSize = data.size() - dataIndex - 2;
+    std::copy(data.begin() + dataIndex, data.begin() + dataIndex + frameDataSize, frameData.begin());
+    dataIndex += frameDataSize;
+
+    uint16_t receivedChecksum = data[dataIndex++] & 0xFF;
+    receivedChecksum |= static_cast<uint16_t>(data[dataIndex++] << 8) & 0xFF00;
+
+    uint16_t expectedChecksum = Checksum::calculateCRC16(data.data(), data.size() - 2);
+
+    checksum = receivedChecksum;
+}
+
+std::array<uint8_t, 256> ModbusAsciiFrame::serialize()
+{
+    std::array<uint8_t, 256> data {};
+    /* TODO */
+    return data;
+}
+
+void ModbusAsciiFrame::deserialize(const std::array<uint8_t, 256>& data)
+{
+    /* TODO */
+}
+
+std::array<uint8_t, 256> ModbusTcpFrame::serialize()
+{
+    std::array<uint8_t, 256> data {};
+    /* TODO */
+    return data;
+}
+
+void ModbusTcpFrame::deserialize(const std::array<uint8_t, 256>& data)
+{
+    /* TODO */
+}
--- src/ModbusFrame.hpp
+++ src/ModbusFrame.hpp
@@ -1,14 +1,91 @@
 #ifndef MODBUSFRAME_HPP
 #define MODBUSFRAME_HPP
 
+#include "Modbus.hpp"
 #include <array>
 #include <cstdint>
+#include <memory>
 
 class ModbusFrame {
 public:
-    virtual ~ModbusFrame() = default;
+    ModbusFunctionCode functionCode;
+    std::array<uint8_t, 256> frameData;
+
     virtual std::array<uint8_t, 256> serialize() = 0;
     virtual void deserialize(const std::array<uint8_t, 256>& data) = 0;
+};
+
+class ModbusRTUFrame : public ModbusFrame {
+public:
+    uint8_t slaveaddr;
+    uint16_t checksum;
+
+    std::array<uint8_t, 256> serialize() override;
+    void deserialize(const std::array<uint8_t, 256>& data) override;
+};
+
+class ModbusAsciiFrame : public ModbusFrame {
+public:
+    uint8_t start;
+    uint8_t address;
+    uint16_t checksum;
+    uint8_t end;
+
+    std::array<uint8_t, 256> serialize() override;
+    void deserialize(const std::array<uint8_t, 256>& data) override;
+};
+
+class MbapHeader {
+public:
+    uint16_t transactionId;
+    uint16_t protocolId;
+    uint16_t length;
+    uint8_t unitId;
+
+    MbapHeader()
+        : transactionId(0)
+        , protocolId(0)
+        , length(0)
+        , unitId(0)
+    {
+    }
+
+    MbapHeader(uint16_t transactionId, uint16_t protocolId, uint16_t length, uint8_t unitId)
+        : transactionId(transactionId)
+        , protocolId(protocolId)
+        , length(length)
+        , unitId(unitId)
+    {
+    }
+};
+
+class ModbusTcpFrame : public ModbusFrame {
+public:
+    MbapHeader mbapHeader;
+
+    std::array<uint8_t, 256> serialize() override;
+    void deserialize(const std::array<uint8_t, 256>& data) override;
+};
+
+class ModbusFrameFactory {
+public:
+    enum class FrameType {
+        RTU,
+        ASCII,
+        TCPIP
+    };
+
+    static std::unique_ptr<ModbusFrame> createFrame(FrameType type)
+    {
+        switch (type) {
+        case FrameType::RTU:
+            return std::make_unique<ModbusRTUFrame>();
+        case FrameType::ASCII:
+            return std::make_unique<ModbusAsciiFrame>();
+        case FrameType::TCPIP:
+            return std::make_unique<ModbusTcpFrame>();
+        }
+    }
 };
 
 #endif
