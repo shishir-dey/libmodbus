d25ced3331b80a090fb40d51cddbefd11648b60c
Shishir Dey <shishirdey241097@gmail.com>
2024-02-24 00:19:01 +0530

    Fix serialization/de-serialization logic

--- src/ModbusFrame.cpp
+++ src/ModbusFrame.cpp
@@ -7,7 +7,13 @@ std::array<uint8_t, 256> ModbusRtuFrame::serialize()
     size_t dataIndex = 0;
 
     data[dataIndex++] = slaveaddr;
-    if (auto* responseFrame = std::get_if<ModbusResponseFrame>(&pdu); responseFrame != nullptr) {
+
+    std::variant<ModbusResponseFrame, ModbusExceptionFrame> responseVariant = pdu;
+
+    switch (responseVariant.index()) {
+    case 0: /* ModbusResponseFrame */
+    {
+        ModbusResponseFrame* responseFrame = std::get_if<ModbusResponseFrame>(&responseVariant);
         data[dataIndex++] = static_cast<uint8_t>(responseFrame->functionCode);
 
         std::copy(responseFrame->frameData.begin(), responseFrame->frameData.end(), data.begin() + dataIndex);
@@ -17,29 +23,41 @@ std::array<uint8_t, 256> ModbusRtuFrame::serialize()
 
         data[dataIndex++] = checksum & 0xFF;
         data[dataIndex++] = (checksum >> 8) & 0xFF;
+        break;
+    }
+    case 1: /* ModbusExceptionFrame */
+    {
+        ModbusExceptionFrame* exceptionFrame = std::get_if<ModbusExceptionFrame>(&responseVariant);
+        break;
+    }
     }
 
     return data;
 }
 void ModbusRtuFrame::deserialize(const std::array<uint8_t, 256>& data)
 {
-    size_t dataIndex = 0;
+    /* assume incoming data is a valid request/response frame */
+    slaveaddr = data[0];
 
-    slaveaddr = data[dataIndex++];
-    if (auto* responseFrame = std::get_if<ModbusResponseFrame>(&pdu); responseFrame != nullptr) {
-        responseFrame->functionCode = static_cast<ModbusFunctionCode>(data[dataIndex++]);
+    // uint16_t receivedChecksum = data[dataIndex - 2] | (data[dataIndex - 1] << 8);
+    // uint16_t calculatedChecksum = Checksum::calculateCRC16(data.data(), dataIndex - 2);
 
-        size_t frameDataSize = data.size() - dataIndex - 2;
-        std::copy(data.begin() + dataIndex, data.begin() + dataIndex + frameDataSize, responseFrame->frameData.begin());
-        dataIndex += frameDataSize;
+    // if (receivedChecksum == calculatedChecksum) {
+    //     ModbusRequestFrame requestFrame;
+    //     requestFrame.functionCode = static_cast<ModbusFunctionCode>(data[1]);
+    //     for (size_t i = 2; i < dataIndex - 2; ++i) {
+    //         requestFrame.frameData[i - 2] = data[i];
+    //     }
+    //     pdu = requestFrame;
+    // }
 
-        uint16_t receivedChecksum = data[dataIndex++] & 0xFF;
-        receivedChecksum |= static_cast<uint16_t>(data[dataIndex++] << 8) & 0xFF00;
-
-        uint16_t expectedChecksum = Checksum::calculateCRC16(data.data(), data.size() - 2);
-
-        checksum = receivedChecksum;
+    /* checksum is disabled */
+    ModbusRequestFrame requestFrame;
+    requestFrame.functionCode = static_cast<ModbusFunctionCode>(data[1]);
+    for (size_t i = 2; i < dataIndex - 2; ++i) {
+        requestFrame.frameData[i - 2] = data[i];
     }
+    pdu = requestFrame;
 }
 
 std::array<uint8_t, 256> ModbusAsciiFrame::serialize()
