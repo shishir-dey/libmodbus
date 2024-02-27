ad3b73f26176fe2d436d1606fd585534f5b9f78a
Shishir Dey <shishirdey241097@gmail.com>
2024-02-27 22:40:11 +0530

    Implement ReadDiscreteInputCommand

--- src/ModbusCommand.cpp
+++ src/ModbusCommand.cpp
@@ -53,3 +53,53 @@ ModbusFrame ReadCoilCommand::execute(ModbusDataModel& data, const ModbusFrame& r
     response.validDataCount = qtyOutput + 1;
     return response;
 }
+
+ModbusFrame ReadDiscreteInputCommand::execute(ModbusDataModel& data, const ModbusFrame& request)
+{
+    /**
+     * Procedure for Reading Discreet Inputs
+     *
+     * 1) Check if the Quantity of Inputs is within the valid range (1 to 2000).
+     *    - If valid, proceed to step 2.
+     *    - If not valid, return ExceptionCode ILLEGAL_DATA_VALUE (03).
+     *
+     * 2) Verify the validity of the Starting Address and the sum of Starting Address + Quantity of Inputs.
+     *    - If both conditions are met, proceed to step 3.
+     *    - If not met, return ExceptionCode ILLEGAL_DATA_ADDRESS (02).
+     *
+     * 3) Perform the Read Discrete Inputs  operation and check for success.
+     *    - If successful, the Modbus Server sends a Modbus Response (mb_rsp).
+     *    - If unsuccessful, return ExceptionCode SLAVE_DEVICE_FAILURE (04).
+     */
+
+    ModbusFrame response = { 0 };
+
+    uint16_t startAddress = (request.frameData[0] << 8) | request.frameData[1];
+    uint16_t qtyInput = (request.frameData[2] << 8) | request.frameData[3];
+
+    if (qtyInput < 1 || qtyInput > 0x7D0) {
+        response.exceptionCode = ModbusExceptionCode::ILLEGAL_DATA_ADDRESS;
+        response.frameType = ModbusFrameType::EXCEPTION;
+        return response;
+    }
+
+    if (startAddress >= 0 && startAddress + qtyInput > ModbusDataModel::MAX_COILS) {
+        response.exceptionCode = ModbusExceptionCode::ILLEGAL_DATA_ADDRESS;
+        response.frameType = ModbusFrameType::EXCEPTION;
+        return response;
+    }
+
+    response.functionCode = ModbusFunctionCode::READ_DISCRETE_INPUTS;
+    uint8_t byteIndex = 1;
+    for (uint16_t index = startAddress; index < startAddress + qtyInput; ++index) {
+        bool temp = data.readDiscreteInput(index);
+        response.frameData[byteIndex] |= (temp << ((index - startAddress) % 8));
+        if ((index - startAddress) % 8 == 7) {
+            byteIndex++;
+        }
+    }
+    response.frameData[0] = static_cast<uint8_t>((qtyInput + 7) / 8);
+    response.frameType = ModbusFrameType::RESPONSE;
+    response.validDataCount = qtyInput + 1;
+    return response;
+}
